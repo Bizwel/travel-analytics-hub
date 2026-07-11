bc2ab7ffb0eae02db221d0ec69deda435a0d7521
Bizwel <134703408+Bizwel@users.noreply.github.com>
2026-07-11 16:33:43 +0100

    Refactor main.jsx to include CSS imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,22 @@
-import { DataProvider } from "./context/DataContext";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <ThemeProvider>
-            <DataProvider>
-                <App />
-            </DataProvider>
-        </ThemeProvider>
-    </StrictMode>
-);
+import { DataProvider } from "./context/DataContext";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import './index.css'
+
+import App from './App.jsx'
+import { ThemeProvider } from './context/ThemeContext'
+import { DataProvider } from './context/DataContext'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </ThemeProvider>
+  </StrictMode>,
+)
