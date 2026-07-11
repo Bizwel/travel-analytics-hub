8efe3846c11faf32dee7cf26b0a5602aee27fd34
Bizwel <134703408+Bizwel@users.noreply.github.com>
2026-07-11 13:57:27 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,7 @@ import './App.css'
 
 function App() {
   return (
-    //<BrowserRouter basename="/travel-analytics-hub">
-      <BrowserRouter basename={import.meta.env.BASE_URL}>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<DashboardLayout />}>
           <Route index element="/" element={<OverviewPage />} />
