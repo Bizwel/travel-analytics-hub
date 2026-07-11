7c4e86d4ec0868a5395860bf446d68b108fe18aa
Bizwel <134703408+Bizwel@users.noreply.github.com>
2026-07-11 18:16:07 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import OverviewPage from "./pages/OverviewPage";
 import DashboardLayout from "./components/DashboardLayout";
 
-function TestPage() {
-  return <h1>Dashboard OK</h1>;
-}
 
 export default function App() {
   return (
     <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route element={<DashboardLayout />}>
-          <Route path="/" element={<TestPage />} />
+         <Route path="/" element={<OverviewPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
