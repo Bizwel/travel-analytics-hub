bf0d2e836c570b3ebf7b63d8ee9f5d1a6821d85f
Bizwel <134703408+Bizwel@users.noreply.github.com>
2026-07-11 14:16:51 +0100

    Wrap App component with DataProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap-icons/font/bootstrap-icons.css'
-import './index.css'
-import App from './App.jsx'
-import { ThemeProvider } from './context/ThemeContext'
+import { DataProvider } from "./context/DataContext";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <ThemeProvider>
+            <DataProvider>
+                <App />
+            </DataProvider>
+        </ThemeProvider>
+    </StrictMode>
+);
